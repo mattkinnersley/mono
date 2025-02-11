bc15ff2daeb02478d7be6eb3f5bf875dccd02dee
Matt Wonlaw <mlaw@roci.dev>
2025-02-11 17:01:43 -0500

    feat(zero-client): composite socket

---
+++ packages/zero-client/src/client/composite-socket.ts
@@ -0,0 +1,6 @@
+export function createSyncMutateSocket() {
+  // mirrors createSocket and creates sync and mutate sockets.
+}
+
+// Implements the components of the socket interface used by Zero
+class CompositeSocket {}
